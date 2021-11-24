e03ce7fb1834c07b31170ff42e8a1b916db1e352
Addis Belete <addis.bel@gmail.com>
2021-11-24 18:27:13 +0300

    Add a components

---
+++ pages/posts/first-posts.js
@@ -0,0 +1,24 @@
+import Link from "next/link"
+import Image from "next/image"
+import Head from "next/head";
+import Layout from "../../components/layout";
+export default function FirstPost() {
+
+	return (
+		<>
+			<Layout >
+				<Head>
+					<title>First Post</title>
+
+				</Head>
+				<h1>First Post</h1>
+				<h2>
+					<Link href="/">
+						<a>Back to Page!</a>
+					</Link>
+				</h2>
+			</Layout>
+		</>
+	)
+
+}
